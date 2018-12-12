4099d5efaf1bfc35708a8bffaae134b312e3a083
HANNSG-PC\Administrator <852110522@qq.com>
2018-12-12 17:37:11 +0800

    fix

---
+++ zhita_daichao_service/src/main/java/com/zhita/util/getPhone.java
@@ -0,0 +1,52 @@
+package com.zhita.util;
+
+import java.security.AlgorithmParameters;
+import java.security.Security;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Base64;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class getPhone {
+	public Object getPhoneNumber(String encryptedData, String iv, String session_key) {
+		// 被加密的数据
+		byte[] dataByte = Base64.decode(encryptedData);
+		// 加密秘钥
+		byte[] keyByte = Base64.decode(session_key);
+		// 偏移量
+		byte[] ivByte = Base64.decode(iv);
+		try {
+			// 如果密钥不足16位，那么就补足. 这个if 中的内容很重要
+			int base = 16;
+			if (keyByte.length % base != 0) {
+				int groups = keyByte.length / base + (keyByte.length % base != 0 ? 1 : 0);
+				byte[] temp = new byte[groups * base];
+				Arrays.fill(temp, (byte) 0);
+				System.arraycopy(keyByte, 0, temp, 0, keyByte.length);
+				keyByte = temp;
+			}
+			// 初始化
+			Security.addProvider(new BouncyCastleProvider());
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+			SecretKeySpec spec = new SecretKeySpec(keyByte, "AES");
+			AlgorithmParameters parameters = AlgorithmParameters.getInstance("AES");
+			parameters.init(new IvParameterSpec(ivByte));
+			cipher.init(Cipher.DECRYPT_MODE, spec, parameters);// 初始化
+			byte[] resultByte = cipher.doFinal(dataByte);
+			if (null != resultByte && resultByte.length > 0) {
+				String result = new String(resultByte, "UTF-8");
+				return JSONObject.parseObject(result);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+}
