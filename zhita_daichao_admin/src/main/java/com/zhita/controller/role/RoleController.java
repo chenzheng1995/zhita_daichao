fed6ab830275dff6671a841df421cb073e5d21fe
MicroWin10-1947\Administrator <springboot_lhq@163.com>
2019-04-11 16:54:13 +0800

    test

--- zhita_daichao_admin/src/main/java/com/zhita/controller/role/RoleController.java
+++ zhita_daichao_admin/src/main/java/com/zhita/controller/role/RoleController.java
@@ -9,7 +9,6 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
