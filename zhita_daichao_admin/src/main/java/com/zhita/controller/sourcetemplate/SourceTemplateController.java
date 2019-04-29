c779a171ae6afff19a9fdd1009ef94a20344ebb8
MicroWin10-1947\Administrator <springboot_lhq@163.com>
2019-04-29 10:52:46 +0800

    test

--- zhita_daichao_admin/src/main/java/com/zhita/controller/sourcetemplate/SourceTemplateController.java
+++ zhita_daichao_admin/src/main/java/com/zhita/controller/sourcetemplate/SourceTemplateController.java
@@ -1,24 +1,18 @@
 package com.zhita.controller.sourcetemplate;
 
-import java.awt.Image;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
-import java.util.Map;
 
-import javax.swing.filechooser.FileNameExtensionFilter;
-
-import org.apache.tomcat.util.http.fileupload.disk.DiskFileItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import com.zhita.model.manage.SourceTemplate;
 import com.zhita.service.sourcetemplate.SourceTemplateService;
