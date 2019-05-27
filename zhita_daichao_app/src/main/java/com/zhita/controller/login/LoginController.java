1e2a2130e072a3413447103b79b1f569a02df9a1
MicroWin10-1303\Administrator <852110522@qq.com>
2019-05-27 11:18:00 +0800

    fix

--- zhita_daichao_app/src/main/java/com/zhita/controller/login/LoginController.java
+++ zhita_daichao_app/src/main/java/com/zhita/controller/login/LoginController.java
@@ -3,6 +3,8 @@ package com.zhita.controller.login;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
